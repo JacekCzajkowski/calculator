87b2c425ccedd62e2523c8cbaa4006158a271056
Jacek <ja_cek@gmx.com>
2021-01-24 21:18:12 +0100

    app.js update

---
+++ assets/scripts/app.js
@@ -0,0 +1,13 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function addNumbers(num1, num2) {
+    const result = num1 + num2;
+    return result;
+} 
+
+currentResult = addNumbers(1, 2);
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
