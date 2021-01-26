e74214d0f7ee0dd41071d853a60e3fbd22066114
Jacek <ja_cek@gmx.com>
2021-01-26 10:43:00 +0100

    JavaScript update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,13 +2,15 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
+// Gets input from input field
 function getUserInput() {
     return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function writeToLog(
